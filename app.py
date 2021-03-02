6668407ed85d72f883b3bf3cfade510447b1db39
Daniel Pyon <daniel@quicksat.com>
2021-03-02 00:02:05 -0800

    Added signup

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ web.config.debug = False
 
 urls = (
 	'/login', 'Login',
-	'/reset', 'Reset'
+	'/reset', 'Reset',
+	'/signup', 'Signup',
 )
+
+# Salt for hashing passwords
+SALT = 'colorlessgreenideas'
 
 DB_USER = config('DBUSER')
 DB_PASS = config('DBPASS')
@@ -37,7 +41,7 @@ class Login:
 		username, password = web.input().username, web.input().password
 		identity = db.select('users', where='user=$username', vars=locals())[0]
 		try:
-			if hashlib.sha1(str('colorlessgreenideas' + password).encode('utf-8')).hexdigest() == identity['pass']:
+			if hashlib.sha1(str(SALT + password).encode('utf-8')).hexdigest() == identity['pass']:
 				session.login = 1
 				session.privilege = identity['privilege']
 				render = create_render(session.privilege)
@@ -60,9 +64,37 @@ class Reset:
 		render = create_render(session.privilege)
 		return render.logout()
 
+class Signup:
+	def GET(self):
+		if logged():
+			render = create_render(session.privilege)
+			return '%s' % render.signup()
+		else:
+			render = create_render(session.privilege)
+			return '%s' % render.signup()
+	def POST(self):
+		form = web.input()
+		email, username, password = form.email, form.username, form.password
+
+		hashed = hashlib.sha1(str(SALT + password).encode('utf-8')).hexdigest()
+		args = {
+			'user': username,
+			'pass': hashed,
+			'email': email
+		}
+		db.insert('users', **args)
+
+		identity = db.select('users', where='user=$username', vars=locals())[0]
+		session.login = 1
+		session.privilege = identity['privilege']
+		
+		render = create_render(session.privilege)
+		return render.login_ok()
+
 app = web.application(urls, globals())
 store = web.session.DiskStore('sessions')
 session = web.session.Session(app, store, initializer={'login': 0, 'privilege': 0})
 
 if __name__ == '__main__':
 	app.run()
+
